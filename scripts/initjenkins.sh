259af587fedac858667ac59f903e02a2b74d496b
Loveneesh Singh <lovensi@thoughtworks.com>
2018-09-25 02:25:33 +0530

    install maven for build

--- scripts/initjenkins.sh
+++ scripts/initjenkins.sh
@@ -7,3 +7,4 @@ cp -a /jenkins-job/pipeline/seedjob.groovy ${JENKINS_HOME}/jobs/
 cp -a /jenkins-job/pipeline/init.groovy ${JENKINS_HOME}/init.groovy.d/
 
 /jenkins-job/scripts/install-jjb.sh
+/jenkins-job/scripts/install-tools.sh
