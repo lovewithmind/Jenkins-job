6b79fd70f86f8d07ffebcee149f8975f14c96e04
Loveneesh Singh <lovensi@thoughtworks.com>
2018-09-26 03:15:55 +0530

    Adds jenkins-config.ini & updates seed job

--- pipeline/seedjob.groovy
+++ pipeline/seedjob.groovy
@@ -6,9 +6,12 @@ job('seed-job') {
                 name('pipeline-definitions')
                 url('PIPELINE_DEFINITIONS')
             }
+            extensions {
+                relativeTargetDirectory('/pipeline')
+            }
         }
     }
     steps{
-        shell("/jenkins-job/scripts/run-jjb.sh")
+        shell("jenkins-jobs --conf /jenkins-job/scripts/jenkins-config.ini update update -r /pipeline/jobs:/pipeline/templates")
     }
 }
